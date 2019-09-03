e8190ced6be025eabb366e53e9d77f8bf112e5b0
Logon Aluno <logonaluno@local.com>
2019-09-03 19:57:51 -0300

    Creating the user component and moving it to the component folder, adding the export command to index.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+//importar todos os components
+
+export * from './list/list.component'
+export * from './user/user.component'
